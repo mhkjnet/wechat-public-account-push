44024185cc3969f3f116ee2b65fc1e2b2d57d706
梦鸿 <74364979+mhkjnet@users.noreply.github.com>
2022-09-02 17:56:03 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,23 +4,23 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wxd96a094e4a4571a6",
 
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "4ffe0dbb30be72c9e56c556a3355b411",
 
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: "1",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-    CALLBACK_TEMPLATE_ID: "",
+    CALLBACK_TEMPLATE_ID: "1",
 
 
     /**
      * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
      * [
      *   {name: '老婆0', id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk"}, 
-     *   {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"},
+     *   {name: '老婆1', id: "ousTH5gX6wsPEXaxhPLEHAP6vVzM"},
      * ]
      */
     USERS: [
@@ -31,7 +31,7 @@ export const config = {
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
-      {name: '自己',id: "5hxOO6VskYv你懂的,填错狗头打烂-ywIZVRMpuXhk"}, 
+      {name: '自己',id: "ousTH5gX6wsPEXaxhPLEHAP6vVzM"}, 
     ],
      
     /**
@@ -41,9 +41,9 @@ export const config = {
     /** 天气相关 */
 
     // 所在省份
-    PROVINCE: "广东",
+    PROVINCE: "云南",
     // 所在城市
-    CITY: "惠州",
+    CITY: "镇雄",
 
     /** 重要节日相关 */
 
